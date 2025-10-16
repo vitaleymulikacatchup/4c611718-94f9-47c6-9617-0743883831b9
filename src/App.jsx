24cc23cfbbf239555581163bff0cac9f1fa57560
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-10-16 16:27:11 +0300

    Update project files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <body>
+      <div className="app-content">
+        <footer className="sticky-footer bg-white">
+    <div className="container my-auto">
+        <div className="copyright text-center my-auto">
+            <span>Copyright © 2008-2025 <a target="_blank" href="http://mkr.org.ua"> ООО "МКР" (http://mkr.org.ua)</a></span></div></div></footer>
+      </div>
+    </body>
+  );
+}
